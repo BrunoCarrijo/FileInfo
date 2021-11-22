068a79c238d7b9c262b22b5e34995a0216474226
Bruno Carrijo <bruno.carrijo1009@gmail.com>
2021-11-21 21:46:14 -0300

    Utilizando o file num codigo simples de teste

---
+++ File/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace Files
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"d:\C#\file1.txt";
+            string targetPath = @"d:\C#\file2.txt";
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
